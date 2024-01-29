0ae548fbcd0da984c0f59808147b9e5a0d645eb7
Faruk Ertürk Business <95526628+ertuerk-faruk-business@users.noreply.github.com>
2024-01-29 15:23:30 +0000

    include content in array

--- src/Models/File.php
+++ src/Models/File.php
@@ -5,4 +5,13 @@ namespace VenderaTradingCompany\LaravelAssets\Models;
 class File extends Asset
 {
     protected $table = 'files';
+
+    public function toArray()
+    {
+        $array = parent::toArray();
+
+        $array['content'] = $this->content();
+
+        return $array;
+    }
 }
